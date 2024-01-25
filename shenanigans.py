34b2f31a5ce881cbb49f9eecd8cdeb8e6e854f79
Carsz <157719740+Carsz@users.noreply.github.com>
2024-01-25 18:16:23 +0200

    Update etchasketch.py

--- shenanigans.py
+++ shenanigans.py
@@ -1,10 +1,10 @@
 import tkinter as ttk
 UpDown = 250
 LeftRight = 250
-colorchoosing = 0  #this is used for changing your line colour
+colorchoosing = 0
 colors = ['black', 'green', 'red', 'pink', 'purple']
 
-root = ttk.Tk() # fereastra
+root = ttk.Tk()
 root.geometry('500x500')
 root.title('Etch-a-Sketch')
 canvas = ttk.Canvas(root, width=500, height=500, bg='white')
@@ -39,11 +39,23 @@ def pluscolor(event):
     else:
         colorchoosing += 1
     print(colors[colorchoosing])
+def clear(event):
+    global UpDown
+    global LeftRight
+    canvas.delete('all')
+    UpDown = 250
+    LeftRight = 250
+def showcoords(event):
+    global UpDown
+    global LeftRight
+    print(f'X = {LeftRight} \nY = {UpDown}')
 root.bind('w', Up)
 root.bind('s', Down)
 root.bind('a', Left)
 root.bind('d', Right)
 root.bind('q', minuscolor)
 root.bind('e', pluscolor)
+root.bind('<space>', clear)
+root.bind('c', showcoords)
 canvas.pack()
 root.mainloop()

77e1d9f99e66b4a0826f105c9d2e3b514f2d3344
Carsz <157719740+Carsz@users.noreply.github.com>
2024-01-25 20:06:42 +0200

    Update shenanigans.py

--- shenanigans.py
+++ shenanigans.py
@@ -3,6 +3,7 @@ UpDown = 250
 LeftRight = 250
 colorchoosing = 0
 colors = ['black', 'green', 'red', 'pink', 'purple']
+brushsize = 3
 
 root = ttk.Tk()
 root.geometry('500x500')
@@ -11,20 +12,20 @@ canvas = ttk.Canvas(root, width=500, height=500, bg='white')
 
 def Up(event):
     global UpDown
-    UpDown -= 1
-    canvas.create_line((LeftRight, UpDown), (LeftRight+1, UpDown+1), fill=colors[colorchoosing])
+    UpDown -= 3
+    canvas.create_line((LeftRight, UpDown), (LeftRight+brushsize, UpDown+brushsize+1), fill=colors[colorchoosing])
 def Down(event):
     global UpDown
-    UpDown += 1
-    canvas.create_line((LeftRight, UpDown), (LeftRight+1, UpDown+1), fill=colors[colorchoosing])
+    UpDown += 3
+    canvas.create_line((LeftRight, UpDown), (LeftRight+brushsize, UpDown+brushsize+1), fill=colors[colorchoosing])
 def Left(event):
     global LeftRight
-    LeftRight -= 1
-    canvas.create_line((LeftRight, UpDown), (LeftRight+1, UpDown+1), fill=colors[colorchoosing])
+    LeftRight -= 3
+    canvas.create_line((LeftRight, UpDown), (LeftRight+brushsize, UpDown+brushsize+1), fill=colors[colorchoosing])
 def Right(event):
     global LeftRight
-    LeftRight += 1
-    canvas.create_line((LeftRight, UpDown), (LeftRight+1, UpDown+1), fill=colors[colorchoosing])
+    LeftRight += 3
+    canvas.create_line((LeftRight, UpDown), (LeftRight+brushsize, UpDown+brushsize+1), fill=colors[colorchoosing])
 def minuscolor(event):
     global colorchoosing
     if colorchoosing == 4:
@@ -49,6 +50,14 @@ def showcoords(event):
     global UpDown
     global LeftRight
     print(f'X = {LeftRight} \nY = {UpDown}')
+def plussize(event):
+    global brushsize
+    brushsize += 1
+    print(f'brush size: {brushsize}')
+def minussize(event):
+    global brushsize
+    brushsize -= 1
+    print(f'brush size: {brushsize}')
 root.bind('w', Up)
 root.bind('s', Down)
 root.bind('a', Left)
@@ -57,5 +66,7 @@ root.bind('q', minuscolor)
 root.bind('e', pluscolor)
 root.bind('<space>', clear)
 root.bind('c', showcoords)
+root.bind('z', minussize)
+root.bind('x', plussize)
 canvas.pack()
 root.mainloop()
